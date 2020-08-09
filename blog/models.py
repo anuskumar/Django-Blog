79e4342492ee632b4eec1ce3a95f68e08546a861
achuthvarghese <acvknr@gmail.com>
2020-08-09 23:18:22 +0530

    Custom model managers

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,22 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+	# A custom Manager extending the base Manager
+	# Overriding the base QuerySet get_queryset() method, returns all posts with the published status
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
+
+	# A method count_published() returns the number of posts with published status
+	def count_published(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published').count()
+
+class AuthorManager(models.Manager):
+	# A custom Manager extending the base Manager
+	# A method active_author_posts() returns all the posts written by authors currently active
+	def active_author_posts(self):
+		return super(AuthorManager, self).get_queryset().filter(author__is_active=True)
+
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
@@ -16,6 +32,15 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status 	= models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+	# The default manager named objects renamed to posts. Multiple managers can be created.
+	# Usage: Posts.posts.all()
+	# posts = models.Manager()
+	
+	# Usage: Post.objects.active_author_posts()
+	objects = AuthorManager()
+	# Usage: Post.published.all() and Post.objects.count_published()
+	published = PublishedManager()
 
 	class Meta:
 		ordering = ('-publish',)
